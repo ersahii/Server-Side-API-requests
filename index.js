176c31d9490d828ba177015885597a00cc192307
Mir Mehraj Mushtaq <mehraj95he@gmail.com>
2024-07-21 23:29:48 +0530

    Using Axios for api calling

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import express from "express";
+import axios from 'axios';
 const app = express();
 const port = 3000;
-app.get('/', (req , res)=>{
-    res.render("home.ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.get('/', async(req , res)=>{
+    try{
+        const response = await axios.get("https://bored-api.appbrewery.com/random");
+        const result = response.data;
+        // console.log(result)
+        res.render("home.ejs" , {data : result});
+    }
+    catch(error){
+        console.log("Failed to make request:" ,error.message);
+        res.render("home.ejs", {error : error.message  } )
+    }
+})
+app.post("/activity" , (req , res)=>{
+    console.log(req.body["activity"] , req.body["noOfPeople"]);
+    res.send("Hello")
 })
 app.listen(port, ()=>{
     console.log(`Server Started at port ${port}`);
